8bf277adc309705bc609610606ce11f380f8a7dc
Marco Calzada <mcalzada@kssoluciones.com.mx>
2019-11-07 18:00:14 -0600

    Agregando clase test de persona y funcionalidad en el metodo de edad

---
+++ src/test/java/com/ks/PersonaTest.java
@@ -0,0 +1,42 @@
+package com.ks;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class PersonaTest extends TestCase
+{
+
+    public void testCalcularIMC()
+    {
+    }
+
+    public void testGeneraDNI()
+    {
+    }
+
+    public void testEsMayorDeEdad()
+    {
+        System.out.println(" ------------ INICIANDO EL TEST DE testEsMayorDeEdad() en la clase " + this.getClass().getName());
+
+        Persona menorDeEdad = new Persona();
+        menorDeEdad.setEdad(15);
+
+        Persona mayorDeEdad = new Persona();
+        mayorDeEdad.setEdad(18);
+
+        Persona adultoMayor = new Persona();
+        adultoMayor.setEdad(80);
+
+        Assert.assertFalse(menorDeEdad.esMayorDeEdad());
+        Assert.assertTrue(mayorDeEdad.esMayorDeEdad());
+        Assert.assertTrue(adultoMayor.esMayorDeEdad());
+    }
+
+    public void testComprobarSexo()
+    {
+    }
+
+    public void testToString1()
+    {
+    }
+}
